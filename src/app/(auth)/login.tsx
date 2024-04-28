26ed9996a3e655bcfc6c14d38ca8b5dd9af834d3
onepercman <onepercman@gmail.com>
2024-04-28 21:50:32 +0700

    🚀 update packages

--- src/app/(auth)/login.tsx
+++ src/app/(auth)/login.tsx
@@ -1,11 +1,32 @@
 import { Button } from "@/components/ui/button";
+import { createStore, useStore } from "@/libs/valtio";
 import { useRouter } from "expo-router";
+import EStorage from "expo-secure-store";
+import { useEffect } from "react";
 import { SafeAreaView, Text, TextInput } from "react-native";
+
+class Todo {
+  count = 1;
+
+  inc() {
+    this.count++;
+  }
+}
+
+const todo = createStore(new Todo());
 
 export default function () {
   const router = useRouter();
 
-  function login() {}
+  useEffect(() => {
+    console.log({ EStorage });
+  }, []);
+
+  const { count } = useStore(todo);
+
+  function login() {
+    todo.inc();
+  }
 
   return (
     <SafeAreaView className="flex h-screen flex-col items-center justify-center gap-4">
@@ -20,7 +41,7 @@ export default function () {
         placeholder="Password"
       />
       <Button className="self-end" onPress={login}>
-        <Text>Login</Text>
+        <Text>Login {count}</Text>
       </Button>
     </SafeAreaView>
   );
