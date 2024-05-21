6c664d6a4696046db50bbbaf5ad845aeb5c1f808
onepercman <onepercman@gmail.com>
2024-05-21 22:40:40 +0700

    🚀 update book query

--- src/app/(root)/explore.tsx
+++ src/app/(root)/explore.tsx
@@ -1,16 +1,16 @@
-import { ExploreList } from "@/components/explore-list"
-import { Button } from "@/components/ui/button"
+import { BookCard } from "@/components/book-card"
 import { Input } from "@/components/ui/input"
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { useCategories } from "@/hooks/use-categories"
 import { useDebounce } from "@/libs/custom-hooks/use-debounce"
 import { Service } from "@/services/app.service"
-import { Entypo } from "@expo/vector-icons"
+import { Entypo, Octicons } from "@expo/vector-icons"
 import { useInfiniteQuery } from "@tanstack/react-query"
 import Constants from "expo-constants"
 import { useLocalSearchParams, useRouter } from "expo-router"
 import { useRef, useState } from "react"
-import { ScrollView, Text, View } from "react-native"
+import { ActivityIndicator, Dimensions, FlatList, ScrollView, Text, View } from "react-native"
+import colors from "tailwindcss/colors"
 
 export default function () {
   const router = useRouter()
@@ -25,7 +25,7 @@ export default function () {
 
   const { data: categories } = useCategories()
 
-  const { data, fetchNextPage } = useInfiniteQuery<
+  const { data, isFetching, isFetchingNextPage, hasNextPage, fetchNextPage } = useInfiniteQuery<
     Paginated<Book>,
     any,
     Paginated<Book>,
@@ -70,35 +70,66 @@ export default function () {
           <Entypo size={16} name="compass" /> Khám phá
         </Text>
       </View>
-      <View className="flex flex-col">
-        <View className="flex flex-row p-4">
-          <Input
-            value={searchText}
-            onChangeText={(e) => setSearchText(e)}
-            className="grow"
-            inputClasses="pr-10"
-            placeholder="Tìm kiếm sách..."
-          />
-          <Text className="absolute right-8 top-[27px] text-muted">
-            <Entypo size={20} name="magnifying-glass" />
-          </Text>
-        </View>
 
-        <Tabs
-          value={category || (categories[0]._id as any)}
-          onChange={(e) => router.replace(`/explore/?category=${e}`)}
-        >
-          <TabsList className="overflow-auto">
-            <ScrollView ref={scrollViewRef} horizontal showsHorizontalScrollIndicator={false}>
-              {categories.map((category, index) => (
-                <TabsTrigger key={index} title={category.name} value={category._id} className="py-4" />
-              ))}
-            </ScrollView>
-          </TabsList>
-        </Tabs>
-        {data?.data.length ? <ExploreList data={data.data} /> : <View></View>}
-        {Number(data?.data.length) < Number(data?.count) ? <Button onPress={() => fetchNextPage()}>More</Button> : null}
+      <View className="flex flex-row p-4">
+        <Input
+          value={searchText}
+          onChangeText={(e) => setSearchText(e)}
+          className="grow"
+          inputClasses="pr-10"
+          placeholder="Tìm kiếm sách..."
+        />
+        <Text className="absolute right-8 top-[27px] text-muted">
+          <Entypo size={20} name="magnifying-glass" />
+        </Text>
       </View>
+
+      <Tabs value={category || (categories[0]._id as any)} onChange={(e) => router.replace(`/explore/?category=${e}`)}>
+        <TabsList className="overflow-auto">
+          <ScrollView ref={scrollViewRef} horizontal showsHorizontalScrollIndicator={false}>
+            {categories.map((category, index) => (
+              <TabsTrigger key={index} title={category.name} value={category._id} className="py-4" />
+            ))}
+          </ScrollView>
+        </TabsList>
+      </Tabs>
+
+      <FlatList
+        onEndReachedThreshold={0.3}
+        onEndReached={function () {
+          if (Number(data?.data.length) < Number(data?.count)) {
+            fetchNextPage()
+          }
+        }}
+        style={{ height: Dimensions.get("window").height - 260 }}
+        numColumns={3}
+        data={data?.data}
+        keyExtractor={(e) => e._id}
+        renderItem={({ item }) => <BookCard data={item} className="w-1/3" />}
+        ListEmptyComponent={
+          isFetching ? (
+            <View className="flex h-64 flex-col">
+              <ActivityIndicator animating color={colors.blue[500]} className="m-auto" />
+            </View>
+          ) : (
+            <View className="flex flex-col items-center gap-3">
+              <Text className="m-auto text-center text-muted">
+                <Octicons name="no-entry" size={16} />
+              </Text>
+              <Text className="text-muted">Không tìm thấy kết quả</Text>
+            </View>
+          )
+        }
+        ListFooterComponent={
+          isFetchingNextPage ? (
+            <View className="flex h-64 flex-col">
+              <ActivityIndicator animating color={colors.blue[500]} className="m-auto" />
+            </View>
+          ) : (
+            <View className="h-32" />
+          )
+        }
+      />
     </View>
   )
 }
